adafa2bab4f14a57f986afb75250b87394084e4c
GaddamNandini <nandini.g3333@gmail.com>
2021-03-27 12:49:18 +0530

    initial commit

---
+++ jQuery-T04-HOL_004/ReplaceMeScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#click').click(function(){
+		
+    	$('p').html($('#ta').val());
+	})
+
+	$('p').click(function(){
+		$('p').html('#<img width="50px" height="50px" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fstock-photos%2Fsmiley-face.html&psig=AOvVaw3gSZrGi0iFjvPoB68sexZ_&ust=1616911472262000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCKs-Xmz-8CFQAAAAAdAAAAABAD">');
+	})
+})
+    	
